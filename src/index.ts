1f6be04e546345dee19451a1480e98481d9e4c9a
mazrean <shunsuke.w04@gmail.com>
2025-03-05 21:23:14 +0900

    install action

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
 import core from "@actions/core";
-import github from "@actions/github";
+
+import path from "path";
+import { exec } from "child_process";
+import { promisify } from "util";
+
+import { install } from "./install";
+
+const execAsync = promisify(exec);
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-} catch (error: any) {
-  core.setFailed(error.message);
+  const binPath = await install();
+  core.addPath(path.dirname(binPath));
+
+  const { stdout, stderr } = await execAsync("gocica -h", {});
+
+  if (stdout) {
+    core.info(stdout);
+  }
+  if (stderr) {
+    core.info(stderr);
+  }
+} catch (error) {
+  const err = error as Error;
+  core.error(`Failed to run: ${error}, ${err.stack}`);
+  core.setFailed(err.message);
 }
---
+++ src/install.ts
@@ -0,0 +1,61 @@
+import * as core from "@actions/core";
+import * as tc from "@actions/tool-cache";
+import os from "os";
+
+/**
+ * Install GoCICa.
+ *
+ * @returns             path to installed binary of GoCICa.
+ */
+export async function install(): Promise<string> {
+  const version = core.getInput("version") || "latest";
+
+  core.info(`Installing GoCICa binary ${version}...`);
+
+  const startedAt = Date.now();
+
+  const assetURL = getAssetURL(version);
+
+  core.info(`Downloading binary ${assetURL} ...`);
+
+  const binPath = await tc.downloadTool(assetURL);
+
+  core.info(`Installed GoCICa into ${binPath} in ${Date.now() - startedAt}ms`);
+
+  return binPath;
+}
+
+function getAssetURL(version: string): string {
+  let ext = "";
+  let platform = os.platform().toString();
+  switch (platform) {
+    case "win32":
+      platform = "Windows";
+      ext = ".exe";
+      break;
+    case "darwin":
+      platform = "Darwin";
+      break;
+    case "linux":
+      platform = "Linux";
+      break;
+    default:
+      throw new Error(`Unsupported platform: ${platform}`);
+  }
+
+  let arch = os.arch();
+  switch (arch) {
+    case "arm64":
+      arch = "arm64";
+      break;
+    case "x64":
+      arch = "x86_64";
+      break;
+    case "x32":
+    case "ia32":
+      arch = "i386";
+      break;
+  }
+
+  return `https://github.com/gocica-go/gocica/releases/download/${version}/gocica_${platform}-${arch}${ext}`;
+}
