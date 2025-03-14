65a42acb605fc94fa6bb793e0445ef1de9a3af16
Shunsuke Wakamatsu <50093633+mazrean@users.noreply.github.com>
2025-03-14 16:12:50 +0900

    Update GoCICa version v0.1.0-alpha6 (#6)
    * Update action.yml and index.ts to set default GoCICa version and remove S3 configuration options
    
    * Fix install function to default to latest GoCICa version and update download URL logic
    
    * Update CI workflow to set GoCICa version to v0.1.0-alpha5 and remove S3 configuration options
    
    * build
    
    * Update GoCICa version to v0.1.0-alpha6 in CI workflow and action.yml
    
    * Remove S3 configuration options from pull request CI workflow

--- src/index.ts
+++ src/index.ts
@@ -16,47 +16,6 @@ function buildFlags(): string[] {
     flags.push("--log-level", logLevel);
   }
 
-  const remote = core.getInput("remote");
-  if (remote) {
-    flags.push("--remote", remote);
-  }
-
-  // S3 settings
-  const s3Region = core.getInput("s3-region");
-  if (s3Region) {
-    flags.push("--s3.region", s3Region);
-  }
-
-  const s3Bucket = core.getInput("s3-bucket");
-  if (s3Bucket) {
-    flags.push("--s3.bucket", s3Bucket);
-  }
-
-  const s3AccessKey = core.getInput("s3-access-key");
-  if (s3AccessKey) {
-    flags.push("--s3.access-key", s3AccessKey);
-  }
-
-  const s3SecretAccessKey = core.getInput("s3-secret-access-key");
-  if (s3SecretAccessKey) {
-    flags.push("--s3.secret-access-key", s3SecretAccessKey);
-  }
-
-  const s3Endpoint = core.getInput("s3-endpoint");
-  if (s3Endpoint) {
-    flags.push("--s3.endpoint", s3Endpoint);
-  }
-
-  const s3DisableSSL = core.getInput("s3-disable-ssl");
-  if (s3DisableSSL && s3DisableSSL.toLowerCase() === "true") {
-    flags.push("--s3.disable-ssl");
-  }
-
-  const s3UsePathStyle = core.getInput("s3-use-path-style");
-  if (s3UsePathStyle && s3UsePathStyle.toLowerCase() === "true") {
-    flags.push("--s3.use-path-style");
-  }
-
   return flags;
 }
 
